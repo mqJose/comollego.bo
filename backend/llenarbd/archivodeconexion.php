76ba8d76b556315c057a067b8202bec5edbbc30a
branimir <brani.espinoza@gmail.com>
2014-11-15 00:11:20 -0400

    Sorting Sorting

--- backend/llenarbd/archivodeconexion.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-require_once 'config.php';
-function obtenerconexion() {
-    $conexionnueva = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die("Problemas con la conexion a la base de datos");
-    return $conexionnueva;
-}
--- backend/llenarbd/recibir sindicato.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conexion = mysql_connect("localhost", "root", "123456") or die("Problemas con la conexion");
-mysql_select_db("vico", $conexion) or die("Problemas en la seleccion de la base de datos");
-mysql_query("INSERT INTO `vico`.`sindicato` (`idsindicato`, `nombre`, `direccion`, `telefono`) VALUES ('$_REQUEST[idsindicato]', '$_REQUEST[nombre]', '$_REQUEST[direccion]', '$_REQUEST[telefono]');", $conexion) or die("Problemas en el select".mysql_error());
-mysql_close($conexion);
-header('Location: crear sindicato.php');
-?>
