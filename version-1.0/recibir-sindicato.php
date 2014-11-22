f9bfeee52561f48b48fcc19ac65f3c4ee8e60638
branimir <brani.espinoza@gmail.com>
2014-11-22 10:47:58 -0400

    Ordenando aun

--- version-1.0/recibir-sindicato.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'archivodeconexion.php';
-$con = obtenerconexion();
-
-mysqli_query($con, "INSERT INTO sindicato (nombre, direccion, telefono) VALUES ('$_REQUEST[nombre]', '$_REQUEST[direccion]', '$_REQUEST[telefono]');") or die("Problemas en el select".mysqli_error($con));
-mysql_close($conexion);
-header('Location: crear-sindicato.php');
-?>
